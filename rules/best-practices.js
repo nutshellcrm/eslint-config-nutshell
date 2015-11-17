75e3c35e1558606001bf5bee0e9ef0c6dea8c93b
Ian VanSchooten <ivanschooten@nutshell.com>
2015-11-17 14:20:52 -0500

    Disallow short forms of type conversions

--- rules/best-practices.js
+++ rules/best-practices.js
@@ -3,14 +3,16 @@
 module.exports = {
     'rules': {
         // Specify curly brace conventions for all control statements
-        'curly'            : [2, 'multi-line'],
+        'curly'               : [2, 'multi-line'],
+        // Disallow the type conversions with shorter notations
+        'no-implicit-coercion': [2],
         // Disallow use of multiple spaces
-        'no-multi-spaces'  : [2],
+        'no-multi-spaces'     : [2],
         // Disallow reassignment of function parameters
-        'no-param-reassign': [2],
+        'no-param-reassign'   : [2],
         // Disallow comparisons where both sides are exactly the same
-        'no-self-compare'  : [2],
+        'no-self-compare'     : [2],
         // Disallow usage of expressions in statement position
-        'no-throw-literal' : [2],
+        'no-throw-literal'    : [2],
     },
 };
