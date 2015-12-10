c029188d77ac69034e3eda21aa056c01788955dd
Ian VanSchooten <ivanschooten@nutshell.com>
2015-12-10 15:19:12 -0500

    Add additional react rules

--- rules/react.js
+++ rules/react.js
@@ -9,6 +9,40 @@ module.exports = {
     ],
     'rules': {
         // Prevent variables used in JSX to be incorrectly marked as unused
-        'react/jsx-uses-vars': [2],
+        'react/jsx-uses-vars'               : [2],
+        // Enforce boolean attributes notation in JSX
+        'react/jsx-boolean-value'           : [2, 'always'],
+        // Enforce the closing bracket location for JSX multiline elements
+        'react/jsx-closing-bracket-location': [2, {'selfClosing': 'after-props', 'nonEmpty': false}],
+        // Disallow spaces inside of curly braces in JSX attributes
+        'react/jsx-curly-spacing'           : [2, 'never'],
+        // Validate props indentation in JSX
+        'react/jsx-indent-props'            : [2, 4],
+        // Limit maximum of props on a single line in JSX
+        'react/jsx-max-props-per-line'      : [2, {'maximum': 3}],
+        // Prevent usage of .bind() and arrow functions in JSX props
+        'react/jsx-no-bind'                 : [2],
+        // Prevent duplicate props in JSX
+        'react/jsx-no-duplicate-props'      : [2],
+        // Disallow undeclared variables in JSX
+        'react/jsx-no-undef'                : [2],
+        // Prevent React to be incorrectly marked as unused
+        'react/jsx-uses-react'              : [2],
+        // Prevent usage of setState in componentDidMount
+        'react/no-did-mount-set-state'      : [2],
+        // Prevent usage of setState in componentDidUpdate
+        'react/no-did-update-set-state'     : [2],
+        // Prevent direct mutation of this.state
+        'react/no-direct-mutation-state'    : [2],
+        // Prevent usage of unknown DOM property
+        'react/no-unknown-property'         : [2],
+        // Prefer es6 class instead of createClass for React Components
+        'react/prefer-es6-class'            : [2],
+        // Prevent missing props validation in a React component definition
+        'react/prop-types'                  : [2],
+        // Prevent extra closing tags for components without children
+        'react/self-closing-comp'           : [2],
+        // Prevent missing parentheses around multilines JSX
+        'react/wrap-multilines'             : [2],
     },
 };
