611d013452b3720ee31494023d7cc47bf52dc749
Ian VanSchooten <ian.vanschooten@gmail.com>
2016-12-29 17:05:20 -0500

    Enable more core “Stylistic” rules

--- rules/errors.js
+++ rules/errors.js
@@ -2,8 +2,6 @@
 
 module.exports = {
     'rules': {
-        // Enforce trailing commas in multiline object literals
-        'comma-dangle'               : [2, 'always-multiline'],
         // Disallow template literal placeholder syntax in regular strings
         'no-template-curly-in-string': [2],
         // Disallow negating the left operand of relational operators
